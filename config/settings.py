0424a315fe2180b0d62a02e9a1ae8b491f658b45
sudam panda <sudampanda496.email@example.com>
2026-07-20 21:13:24 +0530

    Add STATIC_ROOT for Render

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,15 @@ USE_TZ = True
 
 # Static files
 
-STATIC_URL = 'static/'
+# Static Files
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
