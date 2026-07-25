45c3103c85f1aa2801bc91bf243a589672d7bf1d
Sudam panda <sudampanda496@gmail.com>
2026-07-25 20:12:19 +0530

    Modify email backend and default sender address
    Updated email configuration settings for the application.

--- config/settings.py
+++ config/settings.py
@@ -154,19 +154,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Email Configuration
 # ======================================
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
-# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-# EMAIL_TIMEOUT = 20
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
@@ -175,4 +163,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = "myshowapp04@gmail.com"
